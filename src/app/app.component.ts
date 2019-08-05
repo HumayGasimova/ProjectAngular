cced38b843b29a264b7df5e39baf15be1a127c2b
Humay <crypto347@gmail.com>
2019-08-05 10:37:23 +0400

    Cleaning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { NavbarComponent } from './navbar/navbar.component';
+
 import { Component, OnDestroy, OnInit, Input } from '@angular/core';
-import { AngularFireDatabase, AngularFireList,  AngularFireObject } from 'angularfire2/database';
-import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -9,99 +8,15 @@ import { Subscription } from 'rxjs';
  
 })
 export class AppComponent implements OnInit{
-  isChecked = true;
-  courses$;
-  coursePush$: AngularFireList<any[]>;
-  course$;
- // author: any[];
-  author$;
- subscription: Subscription;
+ 
 
-  constructor( private db: AngularFireDatabase) {
-    this.courses$ = db.list('/courses')
-                    .snapshotChanges()
-
-    // this.course$ = db.object('/courses/1') 
-    //               .valueChanges();
-
-    // this.author$ = db.object('/authors/1') 
-    // .valueChanges();
-    
+  constructor() {
+  
   }
 
   ngOnInit(){
-    this.coursePush$ = this.db.list('/courses')
+  
     
   }
 
-
-  add(course){
-    this.coursePush$.push(course.value)
-    course.value='';
-  }
- update(course){
-   this.db.object('/courses/' + course.key)
-   .set({
-      title: course.payload.node_.value_ + "Updated",
-      price: 150
-   })
- }
-
- delete(course){
-  this.db.object('/courses/' + course.key)
-  .remove()
-  .then(x=>console.log("DELETED"));
-}
-
-getContent(){
-  this.db.list('/courses')
-  .snapshotChanges()
-  .subscribe(x => {
-    console.log(x)
-  })
-}
-
-onChange($event){
-  console.log($event)
-}
-  // add(course: HTMLInputElement){
-  //   this.coursePush$.push([{
-  //     name: course.value,
-  //     price: 150,
-  //     isLive: true,
-  //     section: [
-  //       {title: 'Components'},
-  //       {title: 'Directives'},
-  //       {title: 'Template'}
-  //     ]
-  //   }])
-  //   course.value='';
-  // }
-
-
-  //  this.subscription = db.list('/authors/1')
-  //  .valueChanges()
-  //  .subscribe(author => {
-  //    this.author = author;
-  //    console.log(author)
-  //  })
-  // ngOnDestroy(){
-  //   this.subscription.unsubscribe();
-
-  colors = [
-    {
-      id: 1,
-      name: 'Red'
-    },
-    {
-      id: 2,
-      name: 'Blue'
-    },
-    {
-      id: 3,
-      name: 'Green'
-    },
-  ]
-  color=1
- 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,66 +1,28 @@
 import { HttpConfigInterceptor } from './interceptor/httpconfig.interceptor';
-import { AdminAuthGuard } from './services/admin-auth-guard.service';
-import { AuthGuard } from './services/auth-guard.service';
-
-
 import { NoopAnimationsModule, BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MockBackend, MockConnection } from '@angular/http/testing';
-import { AuthService } from './services/auth.service';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { HomeComponent } from './home/home.component';
-import { AppErrorHandler } from './common/app-error-handler';
-import { PostService } from './services/post.service';
 import { ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContactFormComponent } from './contact-form/contact-form.component';
-import { SignupFormComponent } from './signup-form/signup-form.component';
-import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
-import { PostsComponent } from './posts/posts.component';
+
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { DataService } from './services/data.service';
-import { NavbarComponent } from './navbar/navbar.component';
+
 import { RouterModule } from '@angular/router';
-import { GithubProfileComponent } from './github-profile/github-profile.component';
-import { GithubFollowersComponent } from './github-followers/github-followers.component';
-import { AdminComponent } from './admin/admin.component';
-import { LoginComponent } from './login/login.component';
-import { NoAccessComponent } from './no-access/no-access.component';
-import { SignupComponent } from './signup/signup.component';
-import { OrderService } from './services/order.service';
-import { fakeBackendProvider } from './helpers/fake-backend';
+
 import { HttpModule, Http, BaseRequestOptions } from '@angular/http';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { environment } from 'src/environments/environment';
-import { AngularFirestore } from 'angularfire2/firestore';
 
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContactFormComponent,
-    SignupFormComponent,
-    NewCourseFormComponent,
-    PostsComponent,
-    NavbarComponent,
-    GithubProfileComponent,
-    GithubFollowersComponent,
-    HomeComponent,
-    NotFoundComponent,
-    AdminComponent,
-    LoginComponent,
-    NoAccessComponent,
-    SignupComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -71,56 +33,16 @@ import { MatInputModule } from '@angular/material/input';
     HttpModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    BrowserAnimationsModule,
-    MatCheckboxModule,
-    MatRadioModule,
-    MatSelectModule,
-    MatInputModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        component: HomeComponent
-      },
-      {
-        path: 'followers/:id/:username',
-        component: GithubProfileComponent
-      },
-      {
-        path: 'followers',
-        component: GithubFollowersComponent
-      },
-      {
-        path: 'posts',
-        component: PostsComponent
-      },
-      {
-        path: '**',
-        component: NotFoundComponent
-      },
-      // { path: 'admin', component: AdminComponent, canActivate: [AuthGuard, AdminAuthGuard] },
-      // { path: 'login', component: LoginComponent },
-      // { path: 'no-access', component: NoAccessComponent }
-
-    ])
+    BrowserAnimationsModule
+    // RouterModule.forRoot([
+    // ])
   ],
   providers: [
-    PostService,
-    { provide: ErrorHandler, useClass: AppErrorHandler },
-    OrderService,
-    AuthService,
-    AuthGuard,
-    AdminAuthGuard,
-    AngularFirestore,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: HttpConfigInterceptor,
       multi:true
-    },
-
-    //For creating a mock back-end. You don't need these in a real app
-    // fakeBackendProvider,
-    // MockBackend,
-    // BaseRequestOptions
+    }
   ],
   bootstrap: [AppComponent],
 })
