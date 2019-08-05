5786eaf9117d6b8563746f5a1966b77c6f0805ba
Humay <crypto347@gmail.com>
2019-08-05 15:42:34 +0400

    Auth.service.ts added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,7 @@ import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { LoginComponent } from './login/login.component';
+import { AuthService } from './auth.service';
 
 
 
@@ -105,6 +106,7 @@ import { LoginComponent } from './login/login.component';
     ])
   ],
   providers: [
+    AuthService
     // {
     //   provide: HTTP_INTERCEPTORS,
     //   useClass: HttpConfigInterceptor,
