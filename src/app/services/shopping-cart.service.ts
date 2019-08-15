892a6b9768652e000faecc77a291afc31e9e0120
Humay <crypto347@gmail.com>
2019-08-15 09:25:15 +0400

    + and - buttons added

--- src/app/services/shopping-cart.service.ts
+++ src/app/services/shopping-cart.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
 import { Product } from '../models/product';
 import 'rxjs/add/operator/take';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +18,7 @@ export class ShoppingCartService {
       dataCreated: new Date().getTime()
     })
   }
-  private getItem(cartId: string, productId: string){
+  private getItem(cartId: string, productId: string): any {
     return this.db.object('/shopping-carts/' + cartId + '/items/' + productId);
   }
 
