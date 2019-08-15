be405a63b4370d3ad0c859986ed4029f68b1d5a9
Humay <crypto347@gmail.com>
2019-08-15 16:34:50 +0400

    rewriting code (more elegant way)

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -2,22 +2,26 @@ import { ShoppingCartItem } from './shopping-cart-item';
 
 
 export class ShoppingCart {
-  // items: ShoppingCartItem[];
-   constructor(public items: ShoppingCartItem[]) {
-   }
+  items: ShoppingCartItem[]=[];
 
-   get productIds() {
-    return Object.keys(this.items);
-   }
-
-   get totalItemsCount() {
-    let count = 0;
-    let a={};
-    for(let productId in this.items){
-
-      count += this.items[productId].quantity;
-  
+  constructor(public itemsMap: {[productId: string] : ShoppingCartItem}) {
+    for(let productId in itemsMap){
+      this.items.push(itemsMap[productId]);
     }
-      return count;
-   }
+  }
+
+//  get productIds() {
+//   return Object.keys(this.items);
+//  }
+
+  get totalItemsCount() {
+  let count = 0;
+  let a={};
+  for(let productId in this.itemsMap){
+
+    count += this.itemsMap[productId].quantity;
+
+  }
+    return count;
+  }
 }
