875552eb8841cbbe8df2cf493acb32a98f4c6daf
Humay <crypto347@gmail.com>
2019-08-16 17:13:15 +0400

    Shipping form added

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-check-out',
+  selector: 'check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.scss']
 })
-export class CheckOutComponent implements OnInit {
+export class CheckOutComponent {
+  shipping={};
 
   constructor() { }
 
-  ngOnInit() {
-  }
 
+  placeOrder(){
+    console.log(this.shipping);
+  }
 }
