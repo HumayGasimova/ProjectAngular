3ea41001ca4fb39b9950ca47703d9888d862bae8
Humay <crypto347@gmail.com>
2019-08-20 13:22:34 +0400

    AdminOrders page added

--- src/app/admin/admin-orders/admin-orders.component.ts
+++ src/app/admin/admin-orders/admin-orders.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { OrderService } from 'src/app/services/order.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-orders',
   templateUrl: './admin-orders.component.html',
   styleUrls: ['./admin-orders.component.scss']
 })
-export class AdminOrdersComponent implements OnInit {
+export class AdminOrdersComponent implements OnDestroy {
+orders;
+subscription: Subscription;
+  constructor(
+    private orderService: OrderService
+  ) { 
+    this.subscription = orderService.getOrders()
+    .snapshotChanges()
+    .map(changes => {
+      return changes.map(c =>{
+       return ({ key: c.payload.key, ...c.payload.val()})
+      }
+      );
+    })
+    .subscribe(orders => this.orders = orders);
+  }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnDestroy() {
+   this.subscription.unsubscribe();
   }
 
 }
--- src/app/my-orders/my-orders.component.ts
+++ src/app/my-orders/my-orders.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { OrderService } from '../services/order.service';
 
 @Component({
   selector: 'app-my-orders',
@@ -6,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-orders.component.scss']
 })
 export class MyOrdersComponent implements OnInit {
-
-  constructor() { }
+  orders$;
+  constructor(
+    private authService: AuthService,
+    private orderService: OrderService
+  ) { 
+    // this.orders$ = authService.user$.switchMap(u=> orderService.getOrdersByUser(u.uid))
+  }
 
   ngOnInit() {
   }
