21f95789689d9396b0af61ca49823e0fdeb317a4
Humay <crypto347@gmail.com>
2019-08-22 17:54:48 +0400

    SignUp form in Login page added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  public form: FormGroup;
 
-  constructor() { }
+  constructor(
+    public fb: FormBuilder
+  ) { }
 
   ngOnInit() {
   }
