f118ed4862d02148c0033b3a0ae8687c363705e5
Humay <crypto347@gmail.com>
2019-08-26 12:33:51 +0400

    newProductComponent added

--- src/app/new-product/new-product.component.ts
+++ src/app/new-product/new-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-product',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-product.component.scss']
 })
 export class NewProductComponent implements OnInit {
+  public form: FormGroup;
 
-  constructor() { }
+  constructor(
+    public fb: FormBuilder,
+  ) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      title: [null, Validators.required],
+      price: [null, Validators.required],
+      category: [null, Validators.required],
+      imageUrl: [null, Validators.required]
+    })
+    
   }
 
 }
